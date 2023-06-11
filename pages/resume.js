a0631635133094e3bb7fb6e183000ac1bb59b444
camelNotation0425 <55813270+camelNotationsdjkh@users.noreply.github.com>
2023-06-11 08:43:53 -0700

    resume update

--- pages/resume.js
+++ pages/resume.js
@@ -1,11 +1,32 @@
 import StnBtn from "@/components/stnBtn";
+import styles from "../styles/resume.module.css"
 
 export default function ResumePage(){
     return (
         <>
-            <div>
-                <StnBtn text="resume1" link="/" />
+            <div className={styles.headerCon}>
+                <h1>My resumes:</h1>
+                <h3>Below are resumes for different focus types. The one displayed in the iframe is 
+                    a hybrid of all three, and my general engineering resume that I use most commonly.
+                </h3>
             </div>
+            
+            <div className={styles.flexCon}>
+                <StnBtn text="Hardware Focus" link="/" />
+                <StnBtn text="Software Focus" link="/" />
+                <StnBtn text="Non-technical" link="/" />
+            </div>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "20px",
+            }}>
+                <div className={styles.bar}></div>
+            </div>
+            <div className={styles.iframeCon}>
+                <iframe src="\resume\IntakeResume.pdf" width="100%" height="500px"></iframe>
+            </div>
+
         </>
     )
 }
